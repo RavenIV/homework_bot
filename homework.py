8125a6cf55ab1b9fb35b65c87a3064538e246797
RavenIV <torrans5@yandex.ru>
2023-12-14 14:51:57 +0300

    First version passing successfully all the tests

--- homework.py
+++ homework.py
@@ -52,8 +52,11 @@ def check_tokens():
 
 def send_message(bot, message):
     """Отправляет сообщение в Telegram чат."""
-    bot.send_message(TELEGRAM_CHAT_ID, message)
-    logging.debug(f'Бот отправил сообщение: "{message}"')
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+        logging.debug(f'Бот отправил сообщение: "{message}"')
+    except Exception as error:
+        logging.error(f'Сбой в работе программы: {error}')
 
 
 def get_api_answer(timestamp):
@@ -86,31 +89,48 @@ def parse_status(homework):
     """Извлекает из данных о домашней работе её статус."""
     homework_name = homework.get('homework_name')
     status = homework.get('status')
-    if homework_name is None or status is None or status not in HOMEWORK_VERDICTS:
+    if homework_name is None or status is None:
         raise KeyError
-    verdict = HOMEWORK_VERDICTS[status]
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    if status not in HOMEWORK_VERDICTS:
+        raise KeyError
+    else:
+        return f'Изменился статус проверки работы "{homework_name}". {HOMEWORK_VERDICTS[status]}'
 
 
 def main():
     """Основная логика работы бота."""
-
-    ...
+    check_tokens()
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
 
-    ...
+    statuses = set()
 
     while True:
         try:
-
-            ...
+            response = get_api_answer(timestamp)
+            check_response(response)
+            homeworks = response['homeworks']
+            new_statuses = {
+                (homework['homework_name'], homework['status'])
+                for homework in homeworks
+            }
+            updates = new_statuses - statuses
+            if len(updates) > 0:
+                for update in updates:
+                    homework_name = update[0]
+                    for homework in homeworks:
+                        if homework_name == homework['homework_name']:
+                            message = parse_status(homework)
+                            send_message(bot, message)
+            timestamp = response['current_date']
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+            logging.error(message)
+            send_message(bot, message)
+
+        time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
