5b3bd4c69fe1283440165d80ffce2da8720069de
RavenIV <torrans5@yandex.ru>
2023-12-15 18:47:10 +0300

    Update messages

--- homework.py
+++ homework.py
@@ -37,24 +37,38 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
-VERDICT_MESSAGE = (
-    'Изменился статус проверки работы "{name}". {verdict}'
+VERDICT = ('Изменился статус проверки работы "{name}". {verdict}')
+ERROR = 'Сбой в работе программы: {}'
+MISSED_TOKENS = 'Отсутствуют переменные окружения: {}.'
+MESSAGE_SENT_SUCCESSFULLY = 'Бот отправил сообщение: "{}"'
+SEND_MESSAGE_ERROR = 'Ошибка при отправлении в Telegram сообщения: "{}"'
+REQUEST_PARAMETERS = (
+    'Параметры запроса: эндпоинт={0}, headers={1}, params={2}'
 )
-ERROR_MESSAGE = 'Сбой в работе программы: {error}'
+BAD_REQUEST_ERROR = 'Ошибка запроса к API. ' + REQUEST_PARAMETERS
+NOT_OK_STATUS_RESPONSE = ('Запрос к API вернул код ответа "{status}"' 
+                          + REQUEST_PARAMETERS)
+UNEXPECTED_RESPONSE = ('Ответ API вернул ошибку: {name}. ' 
+                       + REQUEST_PARAMETERS)
+RESPONSE_NOT_DICT = 'Ответ API не соответствует типу словаря: {}'
+HOMEWORKS_NOT_IN_RESPONSE = 'В ответе API нет ключа `homeworks`.'
+HOMEWORK_NOT_LIST = ('Тип данных ответа API под ключом `homeworks` '
+                     'не является списком: {}')
+MISSED_HOMEWORK_KEYS = 'В данных о домашней работе нет ожидаемого ключа {}'
+UNEXPERCTED_HOMEWORK_STATUS = 'Неожиданный статус домашней работы: {}'
+NO_NEW_STATUSES = 'В ответе API новые статусы не обнаружены.'
 
 logger = logging.getLogger(__name__)
 
 
 def check_tokens():
     """Проверка наличия обязательных переменных окружения."""
-    tokens_unavailable = []
+    missed_tokens = []
     for name in TOKEN_VARIABLE_NAMES:
         if globals()[name] is None:
-            tokens_unavailable.append(name)
-    if len(tokens_unavailable) != 0:
-        logging.critical(
-            f'Отсутствуют переменные окружения: {tokens_unavailable}.'
-        )
+            missed_tokens.append(name)
+    if len(missed_tokens) != 0:
+        logging.critical(MISSED_TOKENS.format(missed_tokens))
         raise TokenNotFoundError
 
 
@@ -62,64 +76,53 @@ def send_message(bot, message):
     """Отправляет сообщение в Telegram чат."""
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
-        logging.debug(f'Бот отправил сообщение: "{message}"')
+        logging.debug(MESSAGE_SENT_SUCCESSFULLY.format(message))
     except telegram.error.TelegramError:
-        logging.exception(
-            f'Ошибка при отправлении в Telegram сообщения: "{message}"'
-        )
+        logging.exception(SEND_MESSAGE_ERROR.format(message))
 
 
 def get_api_answer(timestamp):
     """Отправляет запрос к API и возвращает данные в json-формате."""
     params = {'from_date': timestamp}
+    request_parameters = [ENDPOINT, HEADERS, params]
     try:
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
     except requests.RequestException:
-        raise BadRequestError(f'Ошибка запроса к API: эндпоинт={ENDPOINT}, '
-                              f'headers={HEADERS}, params={params}')
+        raise BadRequestError(BAD_REQUEST_ERROR.format(*request_parameters))
     else:
         if response.status_code != 200:
-            raise NotOkStatusResponseError(
-                f'Запрос к API (эндпоинт={ENDPOINT}, '
-                f'headers={HEADERS}, params={params}). '
-                f'вернул код ответа: {response.status_code}'
-            )
+            raise NotOkStatusResponseError(NOT_OK_STATUS_RESPONSE.format(
+                *request_parameters, status=response.status_code
+            ))
         response = response.json()
         for name in ('code', 'error'):
             if name in response:
-                raise UnexpectedResponseError(
-                    f'Ответ API: ошибка={response[name]}. '
-                    f'Параметры запроса: эндпоинт={ENDPOINT}, '
-                    f'headers={HEADERS}, params={params}'
-                )
+                raise UnexpectedResponseError(UNEXPECTED_RESPONSE.format(
+                    *request_parameters, name=name
+                ))
         return response
 
 
 def check_response(response):
     """Проверка ответа API."""
     if not isinstance(response, dict):
-        raise TypeError(
-            f'Ответ API не соответствует типу словаря: {type(response)}'
-        )
+        raise TypeError(RESPONSE_NOT_DICT.format(type(response)))
     if 'homeworks' not in response:
-        raise KeyError('В ответе API нет ключа `homeworks`.')
+        raise KeyError(HOMEWORKS_NOT_IN_RESPONSE)
     if not isinstance(response['homeworks'], list):
-        raise TypeError(f'Тип данных ответа API под ключом `homeworks` '
-                        f'не является списком: {type(response["homeworks"])}')
+        raise TypeError(HOMEWORK_NOT_LIST.format(type(response["homeworks"])))
 
 
 def parse_status(homework):
     """Извлекает из данных о домашней работе её статус."""
     for key in ('homework_name', 'status'):
         if key not in homework:
-            raise KeyError(
-                f'В данных о домашней работе нет ожидаемого ключа {key}'
-            )
+            raise KeyError(MISSED_HOMEWORK_KEYS.format(key))
     homework_name = homework['homework_name']
     status = homework['status']
     if status not in HOMEWORK_VERDICTS:
-        raise ValueError(f'Неожиданный статус домашней работы: {status}')
-    return VERDICT_MESSAGE.format(
+        raise ValueError(UNEXPERCTED_HOMEWORK_STATUS.format(status))
+    return VERDICT.format(
         name=homework_name, verdict=HOMEWORK_VERDICTS[status]
     )
 
@@ -147,17 +150,17 @@ def main():
                     send_message(bot, message)
                     homework = last_homework
             else:
-                logging.debug('В ответе API новые статусы не обнаружены.')
+                logging.debug(NO_NEW_STATUSES)
         except Exception as error:
-            new_message_error = ERROR_MESSAGE.format(error=error)
+            new_message_error = ERROR.format(error)
             logging.error(new_message_error)
             if new_message_error != message_error:
                 try:
                     send_message(bot, new_message_error)
                 except telegram.error.TelegramError:
-                    logging.exception(
-                        f'Не удалось отправить сообщение "{new_message_error}"'
-                    )
+                    logging.exception(SEND_MESSAGE_ERROR.format(
+                        new_message_error
+                    ))
                 else:
                     message_error = new_message_error
 
